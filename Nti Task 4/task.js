1fe44805c2dc8089abc5ce78540de1aaed44e3df
yousef-khaled231 <yousefk231600298@gmail.com>
2026-08-10 15:31:58 +0300

    Clean submodules and add all tasks

---
+++ Nti Task 4/task.js
@@ -0,0 +1,42 @@
+// //1
+// function multiply(x,y){
+// return x*y
+// }
+// function add(a,b,func){
+//  return  func(a,b)
+// }
+// function displayResult(result)
+// {
+//  console.log(result);
+
+// }
+// var result=add(2,3,multiply)
+// displayResult(result)
+
+
+// //2
+// function greetUser() {
+//   setTimeout(() => {
+//     console.log("Hello, User!")
+//   }, 3000)
+// }
+// greetUser()
+
+// function countdown() {
+//   let count = 10;
+
+//   var intervalId = setInterval(() => {
+//     console.log(count);
+//     count--
+//     if (count < 6) { 
+//       clearInterval(intervalId)
+//       console.log("Time's up!")
+//     }
+//   }, 1000)
+// }
+// countdown()
+
+// var timeoutId = setTimeout(() => {
+//   console.log("This will be cleared!")
+// }, 5000)
+// clearTimeout(timeoutId)
